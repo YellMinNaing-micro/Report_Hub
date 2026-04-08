09fc9a46e43dd0a2b4cf91a8336808e4ab73e4c0
YellMinNaing-micro <yellminnaing5@gmail.com>
2026-04-08 10:15:20 +0630

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,28 @@ import "@/global.css";
 import React from "react";
 import { Stack } from "expo-router";
 import { AppGluestackProvider } from "@/lib/gluestack-provider";
+import { AuthProvider } from "@/lib/auth-context";
 import { ImageSelectionProvider } from "@/lib/image-selection-context";
 
 export default function RootLayout() {
   return (
     <AppGluestackProvider>
-      <ImageSelectionProvider>
-        <Stack
-          screenOptions={{
-            headerTitleAlign: "center",
-            headerShadowVisible: false,
-            headerStyle: { backgroundColor: "#f8fafc" },
-            contentStyle: { backgroundColor: "#f8fafc" },
-          }}
-        >
-          <Stack.Screen name="index" options={{ title: "Report Hub" }} />
-          <Stack.Screen name="camera" options={{ title: "Capture Image" }} />
-        </Stack>
-      </ImageSelectionProvider>
+      <AuthProvider>
+        <ImageSelectionProvider>
+          <Stack
+            screenOptions={{
+              headerTitleAlign: "center",
+              headerShadowVisible: false,
+              headerStyle: { backgroundColor: "#f8fafc" },
+              contentStyle: { backgroundColor: "#f8fafc" },
+            }}
+          >
+            <Stack.Screen name="index" options={{ title: "Login" }} />
+            <Stack.Screen name="home" options={{ title: "Report Hub" }} />
+            <Stack.Screen name="camera" options={{ title: "Capture Image" }} />
+          </Stack>
+        </ImageSelectionProvider>
+      </AuthProvider>
     </AppGluestackProvider>
   );
 }
