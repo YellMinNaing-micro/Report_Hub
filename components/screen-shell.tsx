44ab444e9237be25766e3813df1e58ee0c2f7b40
YellMinNaing-micro <yellminnaing5@gmail.com>
2026-04-08 16:51:20 +0630

    Update screen-shell.tsx

--- components/screen-shell.tsx
+++ components/screen-shell.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import { KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
+
 import { useTheme } from "@/lib/theme-context";
 
 export function ScreenShell({ children }: PropsWithChildren) {
@@ -14,17 +15,17 @@ export function ScreenShell({ children }: PropsWithChildren) {
       <ScrollView
         className="flex-1"
         style={{ backgroundColor: colors.background }}
-        contentContainerClassName="flex-grow px-4 py-8"
+        contentContainerClassName="flex-grow px-5 pb-8 pt-6"
         showsVerticalScrollIndicator={false}
         keyboardShouldPersistTaps="handled"
         keyboardDismissMode="none"
       >
         <View
-          className="absolute -left-10 top-6 h-32 w-32 rounded-full"
+          className="absolute -left-12 top-10 h-44 w-44 rounded-full"
           style={{ backgroundColor: colors.bubblePrimary }}
         />
         <View
-          className="absolute -right-10 top-40 h-36 w-36 rounded-full"
+          className="absolute -right-16 top-52 h-52 w-52 rounded-full"
           style={{ backgroundColor: colors.bubbleSecondary }}
         />
         <View className="w-full max-w-2xl self-center">{children}</View>
