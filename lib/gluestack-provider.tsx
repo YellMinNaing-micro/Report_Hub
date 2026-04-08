6ffbb974662650174d0ff48ec32dfaa2a379df40
YellMinNaing-micro <yellminnaing5@gmail.com>
2026-04-08 10:20:22 +0630

    Update gluestack-provider.tsx

--- lib/gluestack-provider.tsx
+++ lib/gluestack-provider.tsx
@@ -1,7 +1,12 @@
 import React, { PropsWithChildren } from "react";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export function AppGluestackProvider({ children }: PropsWithChildren) {
-  return <GluestackUIProvider config={config}>{children}</GluestackUIProvider>;
+  return (
+    <SafeAreaProvider>
+      <GluestackUIProvider config={config}>{children}</GluestackUIProvider>
+    </SafeAreaProvider>
+  );
 }
